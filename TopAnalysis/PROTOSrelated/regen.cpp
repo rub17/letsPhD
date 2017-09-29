7da6fdb386a210cef6b65db5e34298abfaf1f6b5
Runyu Bi <runyu.bi@cern.ch>
2017-09-29 18:47:03 +0200

    Generated 4 angles

--- TopAnalysis/PROTOSrelated/regen.cpp
+++ TopAnalysis/PROTOSrelated/regen.cpp
@@ -5,6 +5,36 @@
 #include "TLorentzVector.h"
 #include <stdio.h>
 
+double getAngleCosTheta (TLorentzVector restParticle, TLorentzVector particleOne,TLorentzVector particleTwo) {
+
+    TLorentzVector restFrame = restParticle;
+    TLorentzVector particle1 = particleOne;
+    TLorentzVector particle2 = particleTwo;
+
+    particle1.Boost(-restFrame.BoostVector());
+    particle2.Boost(-restFrame.BoostVector());
+
+    double Angle = cos(particle1.Angle(particle2.Vect()));
+
+    return Angle;
+};
+
+double getAnglePhi (TLorentzVector restParticle, TLorentzVector particleOne,TVector3 x_axis) {
+
+    TLorentzVector restFrame = restParticle;
+    TLorentzVector particle1 = particleOne;
+    TVector3 x = x_axis;
+    TVector3 part;
+    
+    particle1.Boost(-restFrame.BoostVector());
+    part = particle1.Vect();
+    part.SetZ(0);
+    
+    double Angle = part.Angle(x_axis);
+
+    return Angle;
+};
+
 struct substrate{
     int particleId;
     int color;
@@ -99,10 +129,35 @@ void regen()
     std::vector<TLorentzVector> leptons;
     std::vector<TLorentzVector> bottom;
     std::vector<TLorentzVector> top;
-    TLorentzVector currentParticle;
+    std::vector<TLorentzVector> W;
+    std::vector<TLorentzVector> quarkSpec;
+
+    std::vector<double> cosTheta;
+    std::vector<double> phi;
+    std::vector<double> cosThetaStar;
+    std::vector<double> phiStar;
     
+
     for (int i = 0; i < sT.size(); i++) {
         TLorentzVector currentParticle;
+        double numberCosTheta;
+        double numberCosThetaStar;
+        double anglePhi;
+        double anglePhiStar;
+
+        
+        TVector3 T;
+        TVector3 N;
+        TVector3 Wproj;
+        
+        currentParticle.SetPxPyPzE(sT[i].bblvq[4].pX,
+                                   sT[i].bblvq[4].pY,
+                                   sT[i].bblvq[4].pZ,
+                                   sqrt(pow(sT[i].bblvq[4].pX,2)
+                                        +pow(sT[i].bblvq[4].pY,2)
+                                        +pow(sT[i].bblvq[4].pZ,2)));
+        quarkSpec.push_back(currentParticle);
+        
         currentParticle.SetPxPyPzE(sT[i].bblvq[0].pX,
                                    sT[i].bblvq[0].pY,
                                    sT[i].bblvq[0].pZ,
@@ -129,16 +184,69 @@ void regen()
                                         +pow(particleMass(sT[i].bblvq[2].particleId),2)));
         bottom.push_back(currentParticle);
         
-        currentParticle = neutrinos.back() + bottom.back() + leptons.back();
+        currentParticle = neutrinos.back() + bottom.back() + leptons.back(); //Top = Neutrino +
+                                                                             //Bottom  + Lepton
         top.push_back(currentParticle);
+        
+        currentParticle = neutrinos.back() + leptons.back(); // W = N + lepton
+        W.push_back(currentParticle);
+        
+        numberCosTheta = getAngleCosTheta(top.back(),quarkSpec.back(),W.back());
+        cosTheta.push_back(numberCosTheta);
+        
+        N = quarkSpec.back().Vect().Cross(W.back().Vect());  //N = st X q; N-->y;
+        T = W.back().Vect().Cross(N); // T = q X N; T-->x
+        anglePhi = getAnglePhi(top.back(),W.back(),T);// phi = angle between x axis and W's projection on x-y plane.
+        phi.push_back(anglePhi);
+
+        numberCosThetaStar = getAngleCosTheta(W.back(),quarkSpec.back(),leptons.back());
+        cosThetaStar.push_back(numberCosThetaStar);
+        
+        anglePhiStar = getAnglePhi(W.back(),leptons.back(),T);// phi* = angle between x axis and l's projection on x-y plane (W                                         frame).
+        phiStar.push_back(anglePhiStar);
     }
     
-    TH1F *h1 = new TH1F("Top", "Top Events", 1000, 100, 250);
+
+//    TH1F *h1 = new TH1F("Top", "Top Events", 1000, 100, 250);
+//    TH1F *h2 = new TH1F("QValue", "Event Q Value", 100, 100, 1000);
+//
+//    for (int i=0; i < top.size(); i++) {
+//        h1->Fill(top[i].M());
+//        h2->Fill(sT[i].eventQ);
+//    }
+
+    
+    TH1F *h3 = new TH1F("CosTheta", "CosTheta", 50, -1.5, 1.5);
+    TH1F *h4 = new TH1F("Phi", "Phi", 50, -3.5, 3.5);
+    TH1F *h5 = new TH1F("CosThetaStar", "CosThetaStar", 50, -1.5, 1.5);
+    TH1F *h6 = new TH1F("PhiStar", "PhiStar", 50, -3.5, 3.5);
+    
     for (int i=0; i < top.size(); i++) {
-        h1->Fill(top[i].M());
+        h3->Fill(cosTheta[i]);
+        h4->Fill(phi[i]);
+        h5->Fill(cosThetaStar[i]);
+        h6->Fill(phiStar[i]);
     }
-    h1->Draw();
+
+//    TCanvas *c1 = new TCanvas("c1","c1");
+//    h1->Draw();
+//    TCanvas *c2 = new TCanvas("c2","c2");
+//    h2->Draw();
     
+    TCanvas *c3 = new TCanvas("c3","Top Rest Frane");
+    c3->Divide(2,1);
+    c3->cd(1);
+    h3 ->Draw();
+    c3->cd(2);
+    h4->Draw();
 
 
+    TCanvas *c4 = new TCanvas("c4","W Rest Frame");
+    c4->Divide(2,1);
+    c4->cd(1);
+    h5 ->Draw();
+    c4->cd(2);
+    h6->Draw();
 }
+
+
