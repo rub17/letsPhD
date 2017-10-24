eb066b78248af5610dc909c6516d33672cfbebf8
Runyu Bi <runyu.bi@cern.ch>
2017-10-24 19:01:09 +0200

    Regular Updates

--- TopAnalysis/PROTOSrelated/samplePolarized.cpp
+++ TopAnalysis/PROTOSrelated/samplePolarized.cpp
@@ -15,41 +15,46 @@ double getAngleCosTheta (TLorentzVector restParticle, TLorentzVector particleOne
     particle1.Boost(-restFrame.BoostVector());
     particle2.Boost(-restFrame.BoostVector());
 
-    double Angle = particle1.Angle(particle2.Vect());
-    double cosTheta = cos(Angle);
-    
-    return cosTheta;
+    double Angle = cos(particle1.Angle(particle2.Vect()));
+
+    return Angle;
 };
 
-double getAngleCosThetaStar (TLorentzVector restParticle, TLorentzVector particleOne,TLorentzVector particleTwo) {
-    
-    TLorentzVector restFrame = restParticle;
-    TLorentzVector particle1 = particleOne;
-    TLorentzVector particle2 = particleTwo;
-    
-    particle1.Boost(-restFrame.BoostVector());
-    
-    double Angle = particle1.Angle(particle2.Vect());
-    double cosTheta = cos(Angle);
-    
-    return cosTheta;
-};
-
-double getAnglePhi (TLorentzVector restParticle, TLorentzVector particleOne,TLorentzVector z_axis,TVector3 x_axis) { //Due to the relatively complex setup, we will need x-axis in the right frame to be provided for the routine; in other words, it does not boost the last argument input. Double check if the x-axis is boosted in the frame that's described in the first input.
+double getAnglePhi (TLorentzVector restParticle, TLorentzVector particleOne,TLorentzVector z_axis,TLorentzVector x_axis) { //Due to the relatively complex setup, we will need x-axis in the right frame to be provided for the routine; in other words, it does not boost the last argument input. Double check if the x-axis is boosted in the frame that's described in the first input.
 
     TLorentzVector restFrame = restParticle;
     TLorentzVector particle1 = particleOne;
-    TVector3 z = z_axis.Vect();
-    z.SetMag(1);
-    TVector3 x = x_axis;
+    TVector3 z;
+    TVector3 x = x_axis.Vect();
     TVector3 part;
     
     particle1.Boost(-restFrame.BoostVector());
+    z_axis.Boost(-restFrame.BoostVector());
+    z = z_axis.Vect();
     part = particle1.Vect();
-    part = part - part.Dot(z)*z;
+    part = part - part.Dot(z);
     
     double Angle = part.Angle(x);
 
+    return Angle;
+};
+
+TLorentzVector getAnglePhi (TLorentzVector restParticle, TLorentzVector particleOne,TLorentzVector z_axis,TLorentzVector x_axis) { //Due to the relatively complex setup, we will need x-axis in the right frame to be provided for the routine; in other words, it does not boost the last argument input. Double check if the x-axis is boosted in the frame that's described in the first input.
+    
+    TLorentzVector restFrame = restParticle;
+    TLorentzVector particle1 = particleOne;
+    TVector3 z;
+    TVector3 x = x_axis.Vect();
+    TVector3 part;
+    
+    particle1.Boost(-restFrame.BoostVector());
+    z_axis.Boost(-restFrame.BoostVector());
+    z = z_axis.Vect();
+    part = particle1.Vect();
+    part = part - part.Dot(z);
+    
+    double Angle = part.Angle(x);
+    
     return Angle;
 };
 
@@ -131,11 +136,14 @@ double particleMass(int id)
     return 0;
 };
 
-void regen()
+void samplePolarized()
 {
     gROOT->Reset();
     std::ifstream inStream("../Events/tbj.unw");
     std::vector<singleTop> sT;
+    std::random_device seed;
+    std::mt19937 engine(seed);
+    std::uniform_double_distribution flatDist(0,1);
     
     while (!inStream.eof()) {
         singleTop a;
@@ -143,30 +151,33 @@ void regen()
         sT.push_back(a);
     }
 
-    std::vector<TLorentzVector> neutrinos;
-    std::vector<TLorentzVector> leptons;
-    std::vector<TLorentzVector> bottom;
-    std::vector<TLorentzVector> top;
-    std::vector<TLorentzVector> W;
-    std::vector<TLorentzVector> quarkSpec;
-
-    std::vector<double> cosTheta;
-    std::vector<double> phi;
-    std::vector<double> cosThetaStar;
-    std::vector<double> phiStar;
+//    std::vector<TLorentzVector> neutrinos;
+//    std::vector<TLorentzVector> leptons;
+//    std::vector<TLorentzVector> bottom;
+//    std::vector<TLorentzVector> top;
+//    std::vector<TLorentzVector> W;
+//    std::vector<TLorentzVector> quarkSpec;
+//
+//    std::vector<double> cosTheta;
+//    std::vector<double> phi;
+//    std::vector<double> cosThetaStar;
+//    std::vector<double> phiStar;
     
-
+    TF1 *f1 = new TF1("theta","1 + cos(x)",0,M_PI);
+    TRandom *r1 = new TRandom();
     for (int i = 0; i < sT.size(); i++) {
+        TLorentzVector top;
+        TLorentzVector W;
+        TLorentzVector quarkSpec;
+        TLorentzVector leptons;
+        TLorentzVector neutrinos;
         TLorentzVector currentParticle;
         double numberCosTheta;
         double numberCosThetaStar;
         double anglePhi;
         double anglePhiStar;
-
-        
         TVector3 T;
         TVector3 N;
-        TVector3 Z;
         TVector3 Wproj;
         
         currentParticle.SetPxPyPzE(sT[i].bblvq[4].pX,
@@ -175,7 +186,7 @@ void regen()
                                    sqrt(pow(sT[i].bblvq[4].pX,2)
                                         +pow(sT[i].bblvq[4].pY,2)
                                         +pow(sT[i].bblvq[4].pZ,2)));
-        quarkSpec.push_back(currentParticle);
+        quarkSpec = currentParticle;
         
         currentParticle.SetPxPyPzE(sT[i].bblvq[0].pX,
                                    sT[i].bblvq[0].pY,
@@ -183,7 +194,7 @@ void regen()
                                    sqrt(pow(sT[i].bblvq[0].pX,2)
                                    +pow(sT[i].bblvq[0].pY,2)
                                    +pow(sT[i].bblvq[0].pZ,2)));
-        neutrinos.push_back(currentParticle);
+        neutrinos = currentParticle;
         
         currentParticle.SetPxPyPzE(sT[i].bblvq[1].pX,
                                    sT[i].bblvq[1].pY,
@@ -192,7 +203,7 @@ void regen()
                                         +pow(sT[i].bblvq[1].pY,2)
                                         +pow(sT[i].bblvq[1].pZ,2)
                                         +pow(particleMass(sT[i].bblvq[1].particleId),2)));
-        leptons.push_back(currentParticle);
+        leptons = currentParticle;
         
         currentParticle.SetPxPyPzE(sT[i].bblvq[2].pX,
                                    sT[i].bblvq[2].pY,
@@ -201,33 +212,33 @@ void regen()
                                         +pow(sT[i].bblvq[2].pY,2)
                                         +pow(sT[i].bblvq[2].pZ,2)
                                         +pow(particleMass(sT[i].bblvq[2].particleId),2)));
-        bottom.push_back(currentParticle);
+        bottom = currentParticle;
         
-        currentParticle = neutrinos.back() + bottom.back() + leptons.back(); //Top = Neutrino +
-                                                                             //Bottom  + Lepton
-        top.push_back(currentParticle);
+        currentParticle = neutrinos + bottom + leptons; //Top = Neutrino +Bottom  + Lepton
+        top = currentParticle;
         
-        currentParticle = neutrinos.back() + leptons.back(); // W = N + lepton
-        W.push_back(currentParticle);
+        currentParticle = neutrinos + leptons; // W = N + lepton
+        W = currentParticle;
         
-        numberCosTheta = getAngleCosTheta(top.back(),quarkSpec.back(),W.back());
-        cosTheta.push_back(numberCosTheta);
-        
-        N = quarkSpec.back().Vect().Cross(W.back().Vect());  //N = st X q; N-->y;
-        T = W.back().Vect().Cross(N); // T = q X N; T-->x
-        N.SetMag(1);
-        T.SetMag(1);
-        Z = W.back().Vect();
-        Z.SetMag(1);
-        
-        anglePhi = getAnglePhi(top.back(),quarkSpec.back(),W.back(),T);// phi = angle between x axis and W's projection on x-y plane.
-        phi.push_back(anglePhi);
+        numberCosTheta,numberCosThetaStar = cos(f1->GetRandom());
+        anglePhi,anglePhiStar = r1->Uniform(0,2*M_PI);
 
-        numberCosThetaStar = getAngleCosThetaStar(W.back(),leptons.back(),W.back());
-        cosThetaStar.push_back(numberCosThetaStar);
-        
-        anglePhiStar = getAnglePhi(W.back(),leptons.back(),W.back(),T);// phi* = angle between x axis and l's projection on x-y plane (W                                         frame).
-        phiStar.push_back(anglePhiStar);
+//        numberCosTheta = getAngleCosTheta(top.back(),quarkSpec.back(),W.back());
+//        cosTheta.push_back(numberCosTheta);
+//
+//        N = quarkSpec.Vect().Cross(W.back().Vect());  //N = st X q; N-->y;
+//        N.SetMag(1);
+//        T = W.Vect().Cross(N); // T = q X N; T-->x
+//        T.SetMag(1);
+//
+//        anglePhi = getAnglePhi(top,W,T);// phi = angle between x axis and W's projection on x-y plane.
+//        phi.push_back(anglePhi);
+//
+//        numberCosThetaStar = getAngleCosTheta(W.back(),quarkSpec.back(),leptons.back());
+//        cosThetaStar.push_back(numberCosThetaStar);
+//
+//        anglePhiStar = getAnglePhi(W.back(),leptons.back(),T);// phi* = angle between x axis and l's projection on x-y plane (W                                         frame).
+//        phiStar.push_back(anglePhiStar);
     }
     
 
@@ -240,17 +251,13 @@ void regen()
 //    }
 
     
-    TH1F *h3 = new TH1F("CosTheta", "CosTheta", 30, -1.5, 1.5);
-    h3 -> SetMarkerStyle(2);
-    TH1F *h4 = new TH1F("Phi", "Phi", 30, -1.5, 1.5);
-    h4 -> SetMarkerStyle(2);
-    TH1F *h5 = new TH1F("CosThetaStar", "CosThetaStar", 30, -1.5, 1.5);
-    h5 -> SetMarkerStyle(2);
-    TH1F *h6 = new TH1F("PhiStar", "PhiStar", 30, -1.5, 1.5);
-    h6 -> SetMarkerStyle(2);
+    TH1F *h3 = new TH1F("CosTheta", "CosTheta", 50, -1.5, 1.5);
+    TH1F *h4 = new TH1F("Phi", "Phi", 50, -3.5, 3.5);
+    TH1F *h5 = new TH1F("CosThetaStar", "CosThetaStar", 50, -1.5, 1.5);
+    TH1F *h6 = new TH1F("PhiStar", "PhiStar", 50, -3.5, 3.5);
     
     for (int i=0; i < top.size(); i++) {
-        h3->Fill(cosTheta[i]);
+        h3->Fill(1-cosTheta[i]);
         h4->Fill(phi[i]);
         h5->Fill(cosThetaStar[i]);
         h6->Fill(phiStar[i]);
@@ -264,21 +271,17 @@ void regen()
     TCanvas *c3 = new TCanvas("c3","Top Rest Frane");
     c3->Divide(2,1);
     c3->cd(1);
-    h3 ->Draw("P");
-    h3->Fit("pol2");
+    h3 ->Draw();
     c3->cd(2);
-    h4->Draw("P");
-    h4->Fit("gaus");
+    h4->Draw();
 
 
     TCanvas *c4 = new TCanvas("c4","W Rest Frame");
     c4->Divide(2,1);
     c4->cd(1);
-    h5 ->Draw("P");
-    h5->Fit("pol2");
+    h5 ->Draw();
     c4->cd(2);
-    h6->Draw("P");
-    h6->Fit("gaus");
+    h6->Draw();
 }
 
 
